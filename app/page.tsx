4bdfa819a5da9e5ca9eb16620897727743f169d3
arslan7172 <hasanovarslan01.02.08@gmail.com>
2025-10-09 12:54:02 +0800

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to DevFlow!</h1>
+      <h1 className="font-space-grotesk h1-bold">Welcome to DevFlow!</h1>
+      <h1 className="font-inter h1-bold">Welcome to DevFlow!</h1>
+    </>
+  );
 };
 
 export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,59 @@
-
+import { defineConfig } from "eslint/config";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-const config = [
-  {
-    ignores: ["components/ui/**/*"],
-  },
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "standard",
-    // "plugin:tailwindcss/recommended",
-    "prettier"
-  ),
-  {
+export default defineConfig([{
+    extends: compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "standard",
+       // "plugin:tailwindcss/recommended",
+        "prettier",
+        "plugin:import/recommended",
+        "plugin:import/typescript"
+    ),
+
+    plugins: {
+        import: fixupPluginRules(_import),
+    },
+
     rules: {
-      "import/order": [
-        "error",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "index",
-            "object",
-          ],
+        "import/order": ["error", {
+            groups: [
+                "builtin",
+                "external",
+                "internal",
+                ["parent", "sibling"],
+                "index",
+                "object",
+            ],
 
-          "newlines-between": "always",
+            "newlines-between": "always",
 
-          pathGroups: [
-            {
-              pattern: "@app/**",
-              group: "external",
-              position: "after",
+            pathGroups: [{
+                pattern: "@app/**",
+                group: "external",
+                position: "after",
+            }],
+
+            pathGroupsExcludedImportTypes: ["builtin"],
+
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
             },
-          ],
-
-          pathGroupsExcludedImportTypes: ["builtin"],
-
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-        },
-      ],
-      "comma-dangle": "off",
+        }],
     },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    rules: {
-      "no-undef": "off",
-    },
-  },
-];
-
-export default config;
+}]);
