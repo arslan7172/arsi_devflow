13fb9586a9625647b338e61fe66477848ab61c76
arslan7172 <hasanovarslan01.02.08@gmail.com>
2025-10-10 22:30:25 +0800

    set up light and dark themes, shadcn ui, navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import "@/app/globals.css";
 import localFont from "next/font/local";
 import React from "react";
+import ThemeProvider from "@/context/Theme";
+import Navbar from "@/components/navbar";
 
 const interVF = localFont({
   src: "../fonts/InterVF.ttf",
   variable: "--font-inter",
   weight: "100 200 300 400 500 600 700 800 900",
 });
- 
+
 const spaceGrotesk = localFont({
   src: "../fonts/SpaceGrotesk.ttf",
   variable: "--font-space-grotesk",
@@ -17,20 +19,33 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
     icon: "/devflow/public/images/site-logo.svg ",
   },
 };
 
 export default function RootLayout({
-  children, 
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${interVF.variable} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${interVF.variable} ${spaceGrotesk.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold">Welcome to DevFlow!</h1>
       <h1 className="font-space-grotesk h1-bold">Welcome to DevFlow!</h1>
-      <h1 className="font-inter h1-bold">Welcome to DevFlow!</h1>
     </>
   );
 };
